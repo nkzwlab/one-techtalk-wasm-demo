180b84e937f998fb45b52177ad272ac8c12b5522
kino-ma <kino.ma.ms@gmail.com>
2021-05-27 01:34:35 +0900

    [refactor] update

--- wasm-rs/src/lib.rs
+++ wasm-rs/src/lib.rs
@@ -2,40 +2,79 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn demo_main(n: u64) -> u64 {
-    let mut ps = vec![2];
+    let mut gen = PrimeGenerator::new();
 
-    let mut x = 3;
-
-    while x < n {
-        ps.push(x);
-        x = next_prime(&x, &ps);
-    }
-
-    return *ps.last().unwrap();
+    gen.nearest_prime(n)
 }
 
-
-fn next_prime(x: &u64, ps: &Vec<u64>) -> u64 {
-    let mut y = *x;
-    while !is_prime(&y, &ps) {
-        y += 2;
-    }
-    y
+struct PrimeGenerator {
+    primes: Vec<u64>,
+    last: u64,
 }
 
-fn is_prime(x: &u64, ps: &Vec<u64>) -> bool{
-    for p in ps {
-        if x % p == 0 { return false };
+impl PrimeGenerator {
+    pub fn new() -> Self {
+        let primes = vec![2, 3];
+
+        Self {
+            primes,
+            last: 3,
+        }
     }
-    true
+
+    pub fn nearest_prime(&mut self, n: u64) -> u64 {
+        while self.last <= n {
+            self.next();
+        }
+
+        let idx = self.primes.binary_search(&n).unwrap_or_else(|i| i - 1);
+
+        self.primes[idx] as u64
+    }
+
+    fn is_prime(&self, x: u64) -> bool {
+        for p in &self.primes {
+            if x % p == 0 { return false };
+        }
+
+        true
+    }
 }
 
+impl Iterator for PrimeGenerator {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut x = self.last;
+
+        while !self.is_prime(x) {
+            x += 2;
+        }
+
+        self.last = x;
+        self.primes.push(x);
+        Some(x)
+    }
+}
 
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn nearest_primes_is_correct() {
+        let mut gen = PrimeGenerator::new();
+
+        let p = gen.nearest_prime(100);
+        assert_eq!(p, 97);
+    }
+
+    #[test]
+    fn nearest_prime_can_return_n() {
+        let mut gen = PrimeGenerator::new();
+
+        let p = gen.nearest_prime(101);
+        assert_eq!(p, 101);
     }
 }
